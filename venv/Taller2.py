453ffb49f7c8e5e2663e734f5b676e81b2ee1ccb
Ximena <xime.huertas@rocketmail.com>
2018-08-17 14:39:43 -0500

    cambios

--- venv/Taller2.py
+++ venv/Taller2.py
@@ -1,38 +1,98 @@
+list1=[]
+coment = {}
+post = {}
+user = {}
+
 class Comment:
     ide = 0
     desc = ""
+    idp = 0
 
-    def __init__(self,ide,desc):
-        self.ide = ide
-        self.desc = desc
-        self.fecha = fecha
+    def crear_c(self):
+        self.ide = input('digite la identificacion del post: ')
+        self.ide = input ('digite la identificacion del comentario: ')
+        self.desc = input('Digite el comentario: ')
+        coment["idp"] = self.idp
+        coment["ide"] = self.ide
+        coment["desc"] = self.desc
+        list1.append(coment)
 
-    def crear(self):
-        self.ident = input('Digite la identificacion: ')
-        self.nombre = input('Digite el nombre: ')
-        self.paswd = input('Digite la contraseña para el usuario: ')
 
 
 class Post(Comment):
     iden = 0
     titulo = ""
-    fecha = ""
+    idu = 0
+
+
+    def crear_p(self):
+        #self.idu = input('Digite la identificacion del usuario: ')
+        #for z in user:
+        for k, v in futbolistas.items():
+            print("%s -> %s" % (k, v))
+
+            '''if v == self.idu:
+                    self.iden = input('Digite la identificacion del post: ')
+                    self.titulo = input('Digite el contenido: ')
+                    post["idu"] = self.idu
+                    post["ident"] = self.iden
+                    post["nombre"] = self.titulo
+                    list1.append(post)
+                else:
+                    print("El usuariono se encuntra registrado:")'''
+
 
 class User(Post):
     ident = 0
     nombre = ""
-    paswd = ""
 
-    def __init__(self,iden,titulo,fecha, ident, nombre, paswd):
-        Post.__init__(self,iden,titulo, fecha)
-        self.ident = ident
-        self.nombre = nombre
-        self.paswd = paswd
 
-    def crear(self):
+    def crear_u(self):
         self.ident = input('Digite la identificacion: ')
         self.nombre= input('Digite el nombre: ')
-        self.paswd = input('Digite la contraseña para el usuario: ')
+        user["ident"] = self.ident
+        user["nombre"] = self.nombre
+        list1.append(user)
+
+
+print("Menu de opciones")
+print("1. Crear Usuario")
+print("2. Crear Post")
+print("3. Crear Comment")
+print("4. Buscar comment")
+print("5. Salir")
+
+r = input("Digite la opcion escogida: ")
+cons = User()
+while int(r)!= 5:
+
+
+    if int(r)==1:
+        cons.crear_u()
+    elif int(r)==2:
+        cons.crear_p()
+    elif int(r)==3:
+        cons.crear_c()
+    elif int(r)==4:
+        print("Digite el id del comentario: ")
+
+    print("Menu de opciones")
+    print("1. Crear Usuario")
+    print("2. Crear Post")
+    print("3. Crear Comment")
+    print("4. Buscar comment")
+    print("5. Salir")
+    r = input("Digite la opcion escogida: ")
+
+print("Hasta pronto!!!")
+
+
+
+
+#print(list1)
+
+
+
 
 
 
